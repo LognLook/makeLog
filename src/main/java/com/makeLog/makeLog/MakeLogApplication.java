56cf51372967e67585cd30ed573001bcb36e3e07
633jinn <judyg00dy68@gmail.com>
2025-03-18 19:43:58 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/makeLog/makeLog/MakeLogApplication.java
@@ -0,0 +1,13 @@
+package com.makeLog.makeLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makeLog/makeLog/MakeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeLog.makeLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
